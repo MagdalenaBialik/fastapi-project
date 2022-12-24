548b12100f1d6b612f266a5e5f4df556dc53a365
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-12-24 16:05:47 +0700

    added new endpoints to fastapi

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data": {"name": "Hello World"}}
+    return {"data": "Blog list"}
 
 
-@app.get("/about")
-def about():
-    return {"data": {"about page"}}
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    return {"data": {"commets": id}}
