b7903360af6b6a364317d54f09f0d83971d2f35c
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-12-27 11:42:18 +0700

    added database

--- blog/database.py
+++ blog/database.py
@@ -2,9 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(
-    "sqlite:///./blog.db", connection_args={"check_same_thread": False}
-)
+engine = create_engine("sqlite:///./blog.db", connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
